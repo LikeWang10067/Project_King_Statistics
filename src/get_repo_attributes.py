2c2713d041544c8699884cc84c02e60188208493
RickyCowboy <like.wang@mail.utoronto.ca>
2025-03-08 18:37:05 -0500

    refactor: simplify get_repository_attributes logic

--- src/get_repo_attributes.py
+++ src/get_repo_attributes.py
@@ -1,136 +1,25 @@
 import requests
 
-def get_repository_commits(repo_name, github_api_url, org_name):
+def get_repository_attributes(github_api_url, org_name, repo_name, info, headers, page=1, per_page=100):
     """
-    Get commits of a repository
+    Get any valid attributes of a repository
     """
-    #TODO: try to do this without pagination, is there a way to get all commits in one request?
+    #TODO: try to do this without pagination, is there a way to get all attributes in one request?
     # similar to github user experience, where they can see the number of commits in the main page
-    url = f"{github_api_url}/repos/{org_name}/{repo_name}/commits"
-    commits_count = 0
-    page = 1
+    url = f"{github_api_url}/repos/{org_name}/{repo_name}/{info}"
+    if info == "closed_issues":
+        url = f"{github_api_url}/repos/{org_name}/{repo_name}/issues?state=closed"
+    attributes_count = 0
     while True:
-        response = requests.get(url, params={"page": page, "per_page": 100})
+        response = requests.get(url, params={"page": page, "per_page": per_page}, headers=headers)
         if response.status_code != 200:
-            raise Exception(f"Failed to fetch commits for {repo_name}: {response.status_code}")
-        commits = response.json()
-        if len(commits) == 0: # meaning we reach the end of the page
+            raise Exception(f"Failed to fetch {info} for {repo_name}: {response.status_code}")
+        attributes = response.json()
+        if info == "environments":
+            attributes_count = attributes["total_count"]
             break
-        commits_count += len(commits)
-        page += 1
-    return commits_count
-
-def get_repository_contributors(repo_name, github_api_url, org_name):
-    """
-    Get contributors of a repository
-    """
-    #TODO: try to do this without pagination, same as above
-    url = f"{github_api_url}/repos/{org_name}/{repo_name}/contributors"
-    contributors_count = 0
-    page = 1
-    while True:
-        response = requests.get(url, params={"page": page, "per_page": 100})
-        if response.status_code != 200:
-            raise Exception(f"Failed to fetch contributors for {repo_name}: {response.status_code}")
-        contributors = response.json()
-        if len(contributors) == 0: # meaning we reach the end of the page
+        if len(attributes) == 0:
             break
-        contributors_count += len(contributors)
+        attributes_count += len(attributes)
         page += 1
-    return contributors_count
-
-def get_repository_branches(repo_name, github_api_url, org_name):
-    """
-    Get branches of a repository
-    """
-    url = f"{github_api_url}/repos/{org_name}/{repo_name}/branches"
-    branches_count = 0
-    page = 1
-    while True:
-        response = requests.get(url, params={"page": page, "per_page": 100})
-        if response.status_code != 200:
-            raise Exception(f"Failed to fetch branches for {repo_name}: {response.status_code}")
-        branches = response.json()
-        if len(branches) == 0: # meaning we reach the end of the page
-            break
-        branches_count += len(branches)
-        page += 1
-    return branches_count
-
-def get_repository_tags(repo_name, github_api_url, org_name):
-    """
-    Get tags of a repository
-    """
-    url = f"{github_api_url}/repos/{org_name}/{repo_name}/tags"
-    tags_count = 0
-    page = 1
-    while True:
-        response = requests.get(url, params={"page": page, "per_page": 100})
-        if response.status_code != 200:
-            raise Exception(f"Failed to fetch tags for {repo_name}: {response.status_code}")
-        tags = response.json()
-        if len(tags) == 0: # meaning we reach the end of the page
-            break
-        tags_count += len(tags)
-        page += 1
-    return tags_count
-
-def get_repository_releases(repo_name, github_api_url, org_name):
-    """
-    Get releases of a repository
-    """
-    url = f"{github_api_url}/repos/{org_name}/{repo_name}/releases"
-    releases_count = 0
-    page = 1
-    while True:
-        response = requests.get(url, params={"page": page, "per_page": 100})
-        if response.status_code != 200:
-            raise Exception(f"Failed to fetch releases for {repo_name}: {response.status_code}")
-        releases = response.json()
-        if len(releases) == 0: # meaning we reach the end of the page
-            break
-        releases_count += len(releases)
-        page += 1
-    return releases_count
-
-def get_repository_closed_issues(repo_name, github_api_url, org_name):
-    """
-    Get closed issues of a repository
-    """
-    url = f"{github_api_url}/repos/{org_name}/{repo_name}/issues"
-    closed_issues_count = 0
-    page = 1
-    while True:
-        response = requests.get(url, params={"page": page, "per_page": 100, "state": "closed"})
-        if response.status_code != 200:
-            raise Exception(f"Failed to fetch closed issues for {repo_name}: {response.status_code}")
-        issues = response.json()
-        if len(issues) == 0: # meaning we reach the end of the page
-            break
-        closed_issues_count += len(issues)
-        page += 1
-    return closed_issues_count
-
-def get_repository_environments(repo_name, github_api_url, org_name):
-    """
-    Get environments of a repository
-    """
-    url = f"{github_api_url}/repos/{org_name}/{repo_name}/environments"
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch environments for {repo_name}: {response.status_code}")
-    environments = response.json()
-    environments_count = environments["total_count"]
-    return environments_count
-    # environments_count = 0
-    # page = 1
-    # while True:
-    #     response = requests.get(url, params={"page": page})
-    #     if response.status_code != 200:
-    #         raise Exception(f"Failed to fetch environments for {repo_name}: {response.status_code}")
-    #     environments = response.json()
-    #     if len(environments) == 0: # meaning we reach the end of the page
-    #         break
-    #     environments_count += len(environments)
-    #     page += 1
-    # return environments_count
+    return attributes_count
--- src/get_statistics.py
+++ src/get_statistics.py
@@ -1,16 +1,24 @@
 import requests
+import time
 import get_repo_attributes as gra
 
 # basic setting for GitHub api
 GITHUB_API_URL = "https://api.github.com"
 ORG_NAME = "Kaggle"
+GITHUB_TOKEN = "your_github_token"
+HEADERS = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": f"Bearer {GITHUB_TOKEN}"
+}
 
 def get_repositories(org_name):
     """
     Get all repositories in Kaggle
     """
     url = f"{GITHUB_API_URL}/orgs/{org_name}/repos"
-    response = requests.get(url)
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch repositories for {org_name}: {response.status_code}")
     repositories = response.json()
     return repositories
 
@@ -20,20 +28,20 @@ def get_repository_statistics(repo_name):
     """
     ret_stat = {}
     url = f"{GITHUB_API_URL}/repos/{ORG_NAME}/{repo_name}"
-    response = requests.get(url)
+    response = requests.get(url, headers=HEADERS)
     if response.status_code != 200:
         print(f"Error: {response.status_code}")
         return ret_stat
     statistics = response.json()
-    ret_stat["commits"] = gra.get_repository_commits(repo_name, GITHUB_API_URL, ORG_NAME)
+    ret_stat["commits"] = gra.get_repository_attributes(GITHUB_API_URL, ORG_NAME, repo_name, "commits", HEADERS, 1, 500)
     ret_stat["stars"] = statistics["stargazers_count"]
-    ret_stat["contributors"] = gra.get_repository_contributors(repo_name, GITHUB_API_URL, ORG_NAME)
-    ret_stat["branches"] = gra.get_repository_branches(repo_name, GITHUB_API_URL, ORG_NAME)
-    ret_stat["tags"] = gra.get_repository_tags(repo_name, GITHUB_API_URL, ORG_NAME)
+    ret_stat["contributors"] = gra.get_repository_attributes(GITHUB_API_URL, ORG_NAME, repo_name, "contributors", HEADERS)
+    ret_stat["branches"] = gra.get_repository_attributes(GITHUB_API_URL, ORG_NAME, repo_name, "branches", HEADERS)
+    ret_stat["tags"] = gra.get_repository_attributes(GITHUB_API_URL, ORG_NAME, repo_name, "tags", HEADERS)
     ret_stat["forks"] = statistics["forks_count"]
-    ret_stat["releases"] = gra.get_repository_releases(repo_name, GITHUB_API_URL, ORG_NAME)
-    ret_stat["closed_issues"] = gra.get_repository_closed_issues(repo_name, GITHUB_API_URL, ORG_NAME)
-    ret_stat["environments"] = gra.get_repository_environments(repo_name, GITHUB_API_URL, ORG_NAME)
+    ret_stat["releases"] = gra.get_repository_attributes(GITHUB_API_URL, ORG_NAME, repo_name, "releases", HEADERS)
+    ret_stat["closed_issues"] = gra.get_repository_attributes(GITHUB_API_URL, ORG_NAME, repo_name, "closed_issues", HEADERS) #TODO: numbers wrong
+    ret_stat["environments"] = gra.get_repository_attributes(GITHUB_API_URL, ORG_NAME, repo_name, "environments", HEADERS)
     return ret_stat
 
 def get_statistics():
@@ -45,11 +53,11 @@ def get_statistics():
 
     # Get statistics of each repository
     for repository in repositories:
-        # repository_name = repository["name"]
-        repository_name = "docker-python"
+        repository_name = repository["name"]
+        # repository_name = "docker-python"
         print(f"====================== Repository: {repository_name} ======================")
         statistics = get_repository_statistics(repository_name)
-        print(statistics.json())
+        print(statistics)
 
 if __name__ == "__main__":
     #TODO: add command line arguments
