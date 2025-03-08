a7583ac4ab8be0a429ba53dc468f7fe49e6ec867
RickyCowboy <like.wang@mail.utoronto.ca>
2025-03-08 11:56:19 -0500

    feat: added function for count the number of commits for one repo

--- src/get_statistics.py
+++ src/get_statistics.py
@@ -4,11 +4,29 @@ import requests
 GITHUB_API_URL = "https://api.github.com"
 ORG_NAME = "Kaggle"
 
+def get_repository_commits(repo_name):
+    """
+    Get commits of a repository in Kaggle
+    """
+    url = f"{GITHUB_API_URL}/repos/{ORG_NAME}/{repo_name}/commits"
+    commits_count = 0
+    page = 1
+    while True:
+        response = requests.get(url, params={"page": page})
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch commits for {repo_name}: {response.status_code}")
+        commits = response.json()
+        if len(commits) == 0: # meaning we reach the end of the page
+            break
+        commits_count += len(commits)
+        page += 1
+
+    return commits_count
+
 def get_repositories():
     """
     Get all repositories in Kaggle
     """
-    # Get all repositories in Kaggle
     url = f"{GITHUB_API_URL}/orgs/{ORG_NAME}/repos"
     response = requests.get(url)
     repositories = response.json()
@@ -18,14 +36,16 @@ def get_repository_statistics(repo_name):
     """
     Get statistics of a repository in Kaggle
     """
-    # Get statistics of a repository in Kaggle
     ret_stat = {}
     url = f"{GITHUB_API_URL}/repos/{ORG_NAME}/{repo_name}"
     response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        return ret_stat
     statistics = response.json()
-    ret_stat["star"] = statistics["stargazers_count"]
-    ret_stat["fork"] = statistics["forks_count"]
-    ret_stat["watch"] = statistics["subscribers_count"]
+    ret_stat["commits"] = get_repository_commits(repo_name)
+    ret_stat["stars"] = statistics["stargazers_count"]
+    ret_stat["forks"] = statistics["forks_count"]
     return ret_stat
 
 def get_statistics():
