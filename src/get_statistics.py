2631bbd56555d93e6b3b78edb5d98f26412769b2
RickyCowboy <like.wang@mail.utoronto.ca>
2025-03-09 01:21:30 -0500

    bug: fixed closed_issue number wrong counting problem

--- src/get_statistics.py
+++ src/get_statistics.py
@@ -1,5 +1,6 @@
 import requests
-import time
+import statistics as sta
+import json
 
 # basic setting for GitHub api
 GITHUB_API_URL = "https://api.github.com"
@@ -29,15 +30,19 @@ def get_repository_attributes(github_api_url, org_name, repo_name, info, headers
     #TODO: try to do this without pagination, is there a way to get all attributes in one request?
     # similar to github user experience, where they can see the number of commits in the main page
     url = f"{github_api_url}/repos/{org_name}/{repo_name}/{info}"
-    if info == "closed_issues":
-        url = f"{github_api_url}/repos/{org_name}/{repo_name}/issues?state=closed"
     attributes_count = 0
     while page < 10000: # avoid very large number causing loop for a very long time
-        response = requests.get(url, params={"page": page, "per_page": per_page}, headers=headers)
+        params = {"page": page, "per_page": per_page}
+        if info == "closed_issues":
+            url = f"{github_api_url}/search/issues?q=repo:{org_name}/{repo_name}+is:issue+is:closed"
+        response = requests.get(url, headers=headers, params=params)
         if response.status_code != 200:
             raise Exception(f"Failed to fetch {info} for {repo_name}: {response.status_code}")
         attributes = response.json()
         if info == "environments":
+            attributes_count = attributes["total_count"]
+            break
+        if info == "closed_issues":
             attributes_count = attributes["total_count"]
             break
         if len(attributes) == 0:
@@ -84,22 +89,30 @@ def get_statistics():
         repository_name = repository["name"]
         print(f"====================== Repository: {repository_name} ======================")
         statistics = get_repository_statistics(repository_name)
+        print(statistics)
         raw_data[repository_name] = statistics
         for attribute in statistics:
             if attribute not in pure_data:
                 pure_data[attribute] = []
             pure_data[attribute].append(statistics[attribute])
     
-    # Proferm data analysis
+    # Proferm data analysis and save the results into a json file
     print("================================= Data Analysis =================================")
+    statistics_results = {}
     for attribute in pure_data:
         data = pure_data[attribute]
         print(f"Attribute: {attribute}")
-        print(f"Max: {max(data)}")
-        print(f"Min: {min(data)}")
+        # print(f"Max: {max(data)}") 
+        # print(f"Min: {min(data)}")
         print(f"Total: {sum(data)}")
-        print(f"Median: {sorted(data)[len(data)//2]}")
-        print(f"=================================")
+        print(f"Median: {sta.median(sorted(data))}")
+        statistics_results[attribute] = {
+            "total": sum(data),
+            "median": sta.median(sorted(data))
+        }
+    print(f"=================================")
+    with open("statistics_results.json", "w") as file_stream:
+        file_stream.write(json.dumps(statistics_results, indent=4))
 
 
 if __name__ == "__main__":
